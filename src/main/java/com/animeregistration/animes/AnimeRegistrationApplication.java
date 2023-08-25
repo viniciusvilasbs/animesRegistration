c8d9fbaabf8729de01d09b831007e78038e05cc9
viniciusvilasbs <vinyvilas@gmail.com>
2023-08-25 11:37:29 -0300

    Project start

---
+++ src/main/java/com/animeregistration/animes/AnimeRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.animeregistration.animes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animeregistration/animes/AnimeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeregistration.animes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

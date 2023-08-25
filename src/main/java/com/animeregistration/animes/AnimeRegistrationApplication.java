fad84e06d89d80136c283ec3c464ad1739942a91
viniciusvilasbs <vinyvilas@gmail.com>
2023-08-25 16:43:14 -0300

    Add Prometheus and Grafana to the project

--- src/main/java/com/animeregistration/animes/AnimeRegistrationApplication.java
+++ src/main/java/com/animeregistration/animes/AnimeRegistrationApplication.java
@@ -1,7 +1,11 @@
 package com.animeregistration.animes;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimeRegistrationApplication {
@@ -10,4 +14,9 @@ public class AnimeRegistrationApplication {
 		SpringApplication.run(AnimeRegistrationApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
